d2185213848b35e640b311bf272b7efa50a63ae9
dannybenson <dan.s.benson@gmail.com>
2014-04-23 14:46:29 -0500

    configure zipcode map

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,4 +14,40 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require bootstrap
+//= require d3
 //= require_tree .
+
+$(function() {
+
+
+
+var width = 1140,
+    height = 600;
+
+    var path = d3.geo.path();
+
+    var svg = d3.select(".map").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+    queue()
+    .defer(d3.json, "zips_us_topo.json")
+    .await(ready);
+
+    function ready(error, us) {
+      svg.append("g")
+      .attr("class", "counties")
+      .selectAll("path")
+      .data(topojson.feature(us, us.objects.zip_codes_for_the_usa).features)
+      .enter().append("path")
+      .attr("class", "zip")
+      .style("fill", "none")
+      .style("stroke", "#CCC")
+      .style("stroke-width", ".5px")
+      .attr("data-zip", function(d) {return d.properties.zip; })
+      .attr("data-state", function(d) {return d.properties.state; })
+      .attr("data-name", function(d) {return d.properties.name; })
+      .attr("d", path);
+    }
+
+})
